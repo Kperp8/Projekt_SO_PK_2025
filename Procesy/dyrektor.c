60ded2988a9469d705e46d12014225bb04234ed6
Kperp8 <kacper.pustulka@interia.com>
2026-01-12 11:51:20 +0100

    zaczynamy prace nad sygnalami

--- Procesy/dyrektor.c
+++ Procesy/dyrektor.c
@@ -1,4 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+
+// TODO: cleanup(), w normalnym workflow to on bedzie usuwal semafory
+// TODO: wysyłanie sygnałów do procesów
 
 int main(int argc, char **argv)
 {
--- Procesy/rejestr.c
+++ Procesy/rejestr.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+// TODO: obsługa sygnałów SIGINT, 1, 2
+
 int main(int argc, char **argv)
 {
     printf("rejestr\n");
